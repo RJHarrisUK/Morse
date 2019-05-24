6bf4ff16666a864549794593f41e0ad2f8f0e455
RJHarrisUK <rjharrishome@yahoo.co.uk>
2019-05-24 11:24:57 +0100

    updated eng to morse

--- Morse.java
+++ Morse.java
@@ -4,6 +4,13 @@ public class Morse {
 
 	public static void main(String[] args) {
 
+		toEnglish();
+		toMorse();
+
+	}
+
+	public static void toEnglish() {
+
 		HashMap<String, Character> morse = new HashMap<>();
 
 		morse.put(".-", 'a');
@@ -49,6 +56,57 @@ public class Morse {
 		for (int i = 0; i < parts.length; i++) {
 			String a = parts[i];
 			System.out.print(morse.get(a));
+		}
+	}
+
+	public static void toMorse() {
+
+		HashMap<Character, String> morse2 = new HashMap<>();
+
+		morse2.put('a', ".-");
+		morse2.put('b', "-...");
+		morse2.put('c', "-.-");
+		morse2.put('d', "-..");
+		morse2.put('e', ".");
+		morse2.put('f', "..-.");
+		morse2.put('g', "--.");
+		morse2.put('h', "....");
+		morse2.put('i', "..");
+		morse2.put('j', ".---");
+		morse2.put('k', "-.");
+		morse2.put('l', ".-..");
+		morse2.put('m', "--");
+		morse2.put('n', "-.");
+		morse2.put('o', "---");
+		morse2.put('p', ".--.");
+		morse2.put('q', "--.-");
+		morse2.put('r', ".-.");
+		morse2.put('s', "...");
+		morse2.put('t', "-");
+		morse2.put('u', "..-");
+		morse2.put('v', "...-");
+		morse2.put('w', ".--");
+		morse2.put('x', "-..-");
+		morse2.put('y', "-.--");
+		morse2.put('z', "--..");
+		morse2.put('1', ".----");
+		morse2.put('2', "..---");
+		morse2.put('3', "...--");
+		morse2.put('4', "....-");
+		morse2.put('5', ".....");
+		morse2.put('6', "-....");
+		morse2.put('7', "--...");
+		morse2.put('8', "---..");
+		morse2.put('9', "----.");
+		morse2.put('0', "-----");
+		morse2.put(' ', "/");
+
+		String string2 = "hello daily programmer good luck on the challenges today";
+
+		char[] newparts = string2.toCharArray();
+		for (int i = 0; i < newparts.length; i++) {
+			char b = newparts[i];
+			System.out.print(morse2.get(b));
 		}
 	}
 }
